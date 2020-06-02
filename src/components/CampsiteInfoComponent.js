9043c8e9042eebd5bb813d73da0460925319c8fd
Peyton <pcopcoeco@gmail.com>
2020-06-02 11:22:24 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
     constructor(props) {
@@ -34,14 +34,18 @@ class CampsiteInfo extends Component {
     }
     render() {
             if(this.props.campsite) {
-                return (<div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                    </div>);
-            } else {
-                return (<div/>);
+                return (
+                    <div className="container">
+                        <div className="row">
+                            {this.renderCampsite(this.props.campsite)}
+                            {this.renderComments(this.props.campsite.comments)}
+                        </div>
+                    </div>
+                );   
+            }
+            return <div />;
             }
     }
-}
+
 
 export default CampsiteInfo;
